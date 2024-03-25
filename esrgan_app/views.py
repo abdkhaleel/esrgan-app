db24f33a98ccc1169da91dabdf503a4f63c37165
abdkhaleel <khaleelkhadeer.ras@gmail.com>
2024-03-25 11:08:59 +0530

    added esrgan

--- esrgan_app/views.py
+++ esrgan_app/views.py
@@ -5,6 +5,10 @@ from django.core.files.storage import FileSystemStorage
 import cv2
 from cv2 import dnn_superres
 import base64
+import torch
+from RealESRGAN import RealESRGAN
+from PIL import Image
+import numpy as np
 
 def upscale_image(request):
     if request.method == 'POST' and request.FILES['image']:
@@ -14,24 +18,36 @@ def upscale_image(request):
         image_path = fs.save(uploaded_image.name, uploaded_image)
 
         # Super-resolution upscaling
-        super_res = dnn_superres.DnnSuperResImpl_create()
+        # super_res = dnn_superres.DnnSuperResImpl_create()
 
-        # Path to the pre-trained model
-        model_path = 'LapSRN_x8.pb'  # Adjust MODEL_ROOT to your model directory
+        # # Path to the pre-trained model
+        # model_path = 'EDSR_x4.pb'  # Adjust MODEL_ROOT to your model directory
 
-        # Load the pre-trained model
-        super_res.readModel(model_path)
-        super_res.setModel('lapsrn', 8)
+        # # Load the pre-trained model
+        # super_res.readModel(model_path)
+        # super_res.setModel('edsr', 4)
+
+        # super_res.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+        # super_res.setPreferableTarget(cv2.dnn.DNN_BACKEND_CUDA)
 
         # Read the uploaded image
-        img = cv2.imread(settings.MEDIA_ROOT + '/' + image_path)  # Adjust MEDIA_ROOT to your media directory
+        img = Image.open(settings.MEDIA_ROOT + '/' + image_path).convert('RGB') # Adjust MEDIA_ROOT to your media directory
+        img_np = np.array(img)
 
         # Upscale the image
-        upscaled = super_res.upsample(img)
+        # upscaled = super_res.upsample(img)
+
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+        model = RealESRGAN(device, scale=8)
+        model.load_weights('weights/RealESRGAN_x8.pth', download=True)
+
+        upscaled = model.predict(img_np)
+        upscaled_np = np.array(upscaled)
 
         # Convert original and upscaled images to base64
-        _, encoded_original_img = cv2.imencode('.jpg', img)
-        _, encoded_upscaled_img = cv2.imencode('.jpg', upscaled)
+        _, encoded_original_img = cv2.imencode('.jpg', cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR))
+        _, encoded_upscaled_img = cv2.imencode('.jpg', cv2.cvtColor(upscaled_np, cv2.COLOR_RGB2BGR))
         original_img = base64.b64encode(encoded_original_img).decode('utf-8')
         upscaled_img = base64.b64encode(encoded_upscaled_img).decode('utf-8')
 
